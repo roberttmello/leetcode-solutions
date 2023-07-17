b8b71a1f7f4a8b89274b8405370e9e0de55d6fbf
Carlos Robert <crobertdmelo@gmail.com>
2023-07-17 15:12:35 -0300

    Add - Roman to Integer Solution

---
+++ src/013_roman_to_integer.py
@@ -0,0 +1,12 @@
+def romanToInt(str):
+    table = {'I': 1, 'V': 5, 'X':10, 'L':50, 'C':100,'D':500,  'M': 1000}
+    total = 0
+    for i in range(len(str)):
+        if i < len(str) - 1 and table[str[i]] < table[str[i + 1]]:
+            total -= table[str[i]]
+        else:
+            total += table[str[i]]
+        print(total)
+    return total
+
+print(romanToInt('IV'))
