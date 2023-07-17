2fc2af7a9d0579e30b1cbd77a9c430a3eebe1171
Carlos Robert <crobertdmelo@gmail.com>
2023-07-17 13:42:53 -0300

    Add - Palindrome Number Solution

--- src/009_palindrome_number.py
+++ src/009_palindrome_number.py
@@ -1,3 +1,11 @@
+import math
+
 def isPalindrome(num):
-  return
- 
+  num_convert_to_string = str(num)
+  num_reverse = num_convert_to_string[::-1]
+  for i in range(0, math.ceil(len(num_reverse)/2)):
+    if num_reverse[i] != num_convert_to_string[i]:
+      return False
+  return True
+
+print(isPalindrome(10101))
