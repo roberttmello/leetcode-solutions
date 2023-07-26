0b7916e898302c5fb4aa4a9636bed03e71967541
Carlos Robert <crobertdmelo@gmail.com>
2023-07-26 10:22:39 -0300

    Add - 069_sqrtx solutioin

---
+++ src/069_sqrtx.py
@@ -0,0 +1,6 @@
+import math
+
+def my_sqrt(x: int) -> int:
+  return math.floor(math.sqrt(x))
+
+print(math.floor(my_sqrt(9)))
---
+++ src/369_valide_perfect_square.py
@@ -0,0 +1,5 @@
+import math
+
+def isPerfectSquare(num: int) -> bool:
+  pass
+
