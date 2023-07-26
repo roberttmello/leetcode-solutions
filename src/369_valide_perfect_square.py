9016a59a4f8b2b90b22d7edd2163a7d504fb0d8e
Carlos Robert <crobertdmelo@gmail.com>
2023-07-26 13:42:09 -0300

    Add - 369 Valide Perfect Square solution

--- src/369_valide_perfect_square.py
+++ src/369_valide_perfect_square.py
@@ -1,5 +1,12 @@
 import math
 
-def isPerfectSquare(num: int) -> bool:
-  pass
+def is_perfect_square(num: int) -> bool:
+  value = int(str(math.sqrt(num)).split('.')[0])
+  return True if value * value == num else False
+print(is_perfect_square(9))
+print(is_perfect_square(8))
+print(is_perfect_square(16))
+print(is_perfect_square(25))
+print(is_perfect_square(144))
+print(is_perfect_square(50))
 
